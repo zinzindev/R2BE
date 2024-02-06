68948f4ed254d8ed43247b43ba40421d9d030e4b
zinzindev <115909025+zinzindev@users.noreply.github.com>
2024-02-07 01:21:40 +0700

    Delete Day 11 (Server Side Framework p2) directory

--- Day 11 (Server Side Framework p2)/my-next-app/src/app/api/blog/[slug]/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET(request, { params }) {
-	// display the params
-	console.log(params.slug);
-	return Response.json({ message: 'Hello form the API', params: params.slug });
-}
--- Day 11 (Server Side Framework p2)/my-next-app/src/app/api/route.js
+++
@@ -1,4 +0,0 @@
-export async function GET() {
-	console.log('Hello World');
-	return Response.json({ hello: 'world' });
-}
